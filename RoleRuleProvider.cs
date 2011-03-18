e4286e304cd43627985542e34c5c7c964924d170
Zachary Gramana <z.gramana@me.com>
2011-03-18 10:26:46 -0400

    Initial Import.

---
+++ RoleRuleProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Orchard.ContentManagement;
+using Orchard.Roles.Models;
+using Orchard.Security;
+using Orchard.Widgets.Services;
+
+namespace PCG.RoleLayer
+{
+    public class RoleRuleProvider : IRuleProvider
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public RoleRuleProvider(IAuthenticationService authenticationService) {
+            _authenticationService = authenticationService;
+        }
+
+        public void Process(RuleContext ruleContext) { 
+            if (!String.Equals(ruleContext.FunctionName, "role", StringComparison.OrdinalIgnoreCase)) {
+                return;
+            }
+
+            var user = _authenticationService.GetAuthenticatedUser();
+            if (user == null) {
+                ruleContext.Result = false;
+                return;
+            }
+
+            var roles = ruleContext.Arguments.Cast<String>();
+            var userRoles = user.As<UserRolesPart>().Roles;
+            var matches = userRoles.Intersect(roles, StringComparer.OrdinalIgnoreCase).Count();
+            if (matches < 1)
+            {
+                ruleContext.Result = false;
+                return;
+            }
+
+            ruleContext.Result = true;
+            return;
+        }
+    }
+}
